70b63aac67d8735b958bd0e53b196a3ba39833e8
Hiba Trifi <trifi.hiba.solicode@gmail.com>
2023-01-20 14:36:28 +0000

    k

---
+++ index.php
@@ -0,0 +1,363 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+    <title>Document</title>
+</head>
+
+<body class="" style="background-color: whitesmoke;" >
+    <!-- Nav bar -->
+    <nav style="background-color: #870036;" class="navbar sticky-top mb-5 ">
+        <div class="container-fluid top-0 start-50 d-flex justify-content-center">
+            <a class="navbar-brand " href="#"><img class=" " src="./img/logo.png" alt=""></a>
+        </div>
+    </nav>
+   
+    <!-- container for tabeles -->
+    <div class="container">
+        <!-- table 1 -->
+        <form method="post" action="index.php">
+            <div class="table-responsive{-sm|-md|-lg|-xl|-xxl}">
+                <table class="table text-center align-middle table-bordered table-hover ">
+                    <thead>
+                        <tr>
+                            <th class="w-25  text-light  bg-dark  ">23 NOV 11:00 H</th>
+                            <th class="w-25  ">Final</th>
+                            <th class="w-25   text-light  bg-dark ">Match</th>
+                        </tr>
+                    </thead>
+                    <tbody class="table-group-divider">
+                        <tr>
+                            <td class="bg-light"><img class="img-fluid w-25" src="./img/mr.png"> Morocco </td>
+                            <td class=" text-light  bg-dark"><input class="w-25 rounded text-light  text-light  bg-dark" type="number" min="0" value="0" name="morocco-game1"> | <input class="w-25 rounded text-light  text-light  bg-dark" type="number" min="0" value="0" name="crotia-game1"> </td>
+                            <td class="bg-light">Croitia<img class="img-fluid w-25" src="./img/cr.png"></td>
+                        </tr>
+                    </tbody>
+                </table>
+                <table class="table text-center align-middle table-bordered table-hover ">
+                    <thead>
+                        <tr>
+                            <th class="w-25  text-light  bg-dark">23 NOV 20:00 H</th>
+                            <th class="w-25 ">Final</th>
+                            <th class="w-25  text-light  bg-dark">Match</th>
+                        </tr>
+                    </thead>
+                    <tbody class="table-group-divider">
+                        <tr>
+                            <td class="bg-light"><img class="img-fluid w-25" src="./img/blg.png"> Belgium </td>
+                            <td class=" text-light  bg-dark"><input class="w-25  rounded text-light  text-light  bg-dark" type="number" min="0" value="0" name="belgium-game1"> | <input class="w-25  rounded text-light  text-light  bg-dark" type="number" min="0" value="0" name="canda-game1"></td>
+                            <td class="bg-light">Canada<img class="img-fluid w-25" src="./img/cnd.png"></td>
+                        </tr>
+                    </tbody>
+                </table>
+                <table class="table text-center align-middle table-bordered table-hover ">
+                    <thead>
+                        <tr>
+                            <th class="w-25  text-light  bg-dark">27 NOV 14:00 H</th>
+                            <th class="w-25 ">Final</th>
+                            <th class="w-25  text-light  bg-dark">Match</th>
+                        </tr>
+                    </thead>
+                    <tbody class="table-group-divider">
+                        <tr>
+                            <td class="bg-light"><img class="img-fluid w-25" src="./img/blg.png"> Belgium </td>
+                            <td class=" text-light  bg-dark"><input class="w-25  rounded text-light  text-light  bg-dark" type="number" min="0" value="0" name="belgium-game2"> | <input class="w-25  rounded text-light  text-light  bg-dark" type="number" min="0" value="0" name="morocco-game2"></td>
+                            <td class="bg-light">Morocco<img class="img-fluid w-25" src="./img/mr.png" ></td>
+                        </tr>
+                    </tbody>
+                </table>
+                <table class="table text-center align-middle table-bordered table-hover ">
+                    <thead>
+                        <tr>
+                            <th class="w-25  text-light  bg-dark">27 NOV 17:00 H</th>
+                            <th class="w-25 ">Final</th>
+                            <th class="w-25  text-light  bg-dark">Match</th>
+                        </tr>
+                    </thead>
+                    <tbody class="table-group-divider">
+                        <tr>
+                            <td class="bg-light"><img class="img-fluid w-25"  src="./img/cr.png"> croatia </td>
+                            <td class=" text-light  bg-dark"><input class="w-25 rounded text-light  text-light  bg-dark" type="number" min="0" value="0" name="croatia-game2"> | <input class="w-25 rounded text-light  text-light  bg-dark" type="number" min="0" value="0" name="canada-game2"></td>
+                            <td class="bg-light">Canada<img class="img-fluid w-25" src="./img/cnd.png"></td>
+                        </tr>
+                    </tbody>
+                </table>
+                <table class="table text-center align-middle table-bordered table-hover ">
+                    <thead>
+                        <tr>
+                            <th class="w-25  text-light  bg-dark">01 DEC 16:00 H</th>
+                            <th class="w-25 ">Final</th>
+                            <th class="w-25  text-light  bg-dark">Match</th>
+                        </tr>
+                    </thead>
+                    <tbody class="table-group-divider">
+                        <tr>
+                            <td class="bg-light"><img class="img-fluid w-25" src="./img/cr.png"> Croitia </td>
+                            <td class=" text-light  bg-dark"><input class="w-25  rounded text-light  text-light  bg-dark" type="number" min="0" value="0" name="croitia-game3"> | <input class="w-25  rounded text-light  text-light  bg-dark" name="belgium-game3" type="number" min="0" value="0"></td>
+                            <td class="bg-light">Belgium<img class="img-fluid w-25" src="./img/blg.png"></td>
+                        </tr>
+                    </tbody>
+                </table>
+                <table class="table text-center align-middle table-bordered table-hover ">
+                    <thead>
+                        <tr>
+                            <th class="w-25  text-light  bg-dark">01 DEC 16:00 H</th>
+                            <th class="w-25">Final</th>
+                            <th class="w-25  text-light  bg-dark">Match</th>
+                        </tr>
+                    </thead>
+                    <tbody class="table-group-divider">
+                        <tr>
+                            <td class="bg-light"><img class="img-fluid w-25" src="./img/cnd.png"> Canada </td>
+                            <td class=" text-light  bg-dark"><input class="w-25  rounded text-light  text-light  bg-dark" type="number" min="0" value="0" name="canda-game3"> | <input class="w-25  rounded text-light  text-light  bg-dark" type="number" min="0" value="0" name="morocco-game3"></td>
+                            <td class="bg-light">Morocco<img class="img-fluid w-25" src="./img/mr.png"></td>
+                        </tr>
+                    </tbody>
+                </table>
+                <div class="d-flex flex-wrap align-center justify-content-center ">
+                    <button class="btn p-2 btn-outline-light rounded " type="submit" style="background-color: #c2165e;" class="btn btn-primary">submit</button>
+                </div>
+            </div>
+        </form>
+        <!-- php -->
+        <?php
+
+   ////////// Varialbels ////////////
+   $morocco_pts = 0;
+   $croatia_pts = 0;
+   $belgium_pts = 0;
+   $canada_pts = 0;
+
+   $morocco_win = 0 ;
+   $croatia_win = 0 ;
+   $belgium_win = 0 ;
+   $canada_win = 0 ;
+
+   $morocco_emp = 0 ;
+   $croatia_emp = 0 ;
+   $belgium_emp = 0 ;
+   $canada_emp = 0 ;
+
+   $morocco_lost = 0 ;
+   $croatia_lost = 0 ;
+   $belgium_lost = 0 ;
+   $canada_lost = 0 ;
+
+   $morocco_GF = 0 ;
+   $croatia_GF = 0 ;
+   $belgium_GF = 0 ;
+   $canada_GF = 0 ;
+
+   $par = 0 ;
+
+//////////// Inputs //////////////     
+   $input1_morocco_game1 = $_POST['morocco-game1'];
+   $input2_crotia_game1 = $_POST['crotia-game1'];
+   $input3_belgium_game1 = $_POST['belgium-game1'];
+   $input4_canda_game1 = $_POST['canda-game1'];
+   $input5_belgium_game2 = $_POST['belgium-game2'];
+   $input6_morocco_game2 = $_POST['morocco-game2'];
+   $input7_croatia_game2 = $_POST['croatia-game2'];
+   $input8_canada_game2 = $_POST['canada-game2'];
+   $input9_croitia_game3 = $_POST['croitia-game3'];
+   $input10_belgium_game3 = $_POST['belgium-game3'];
+   $input11_canda_game3 = $_POST['canda-game3'];
+   $input12_morocco_game3 = $_POST['morocco-game3'];
+ 
+
+////////////////////////// Calculations /////////////////////////
+       
+        if ((empty($_POST['morocco-game1']) && empty($_POST['crotia-game1'])) || (empty($_POST['belgium-game1']) && empty($_POST['canda-game1'])) || (empty($_POST['belgium-game2']) && empty($_POST['morocco-game2'])) || (empty($_POST['croatia-game2']) && empty($_POST['canada-game2'])) || (empty($_POST['croitia-game3']) && empty($_POST['belgium-game3'])) || (empty($_POST['canda-game3']) && empty($_POST['morocco-game3']))) {
+            die("<script>alert('Fill the at least one input in each match ')</script>'");
+        } else{
+           
+            // Game 1
+        if ($input1_morocco_game1 < $input2_crotia_game1) {
+            $croatia_pts = $croatia_pts + 3;
+            $croatia_win = $croatia_win + 1 ;
+            $morocco_lost = $morocco_lost  + 1 ;
+            $par = 3 ;
+        } else if ($input1_morocco_game1 == $input2_crotia_game1) {
+            $morocco_pts = $morocco_pts + 1;
+            $croatia_pts = $croatia_pts + 1;
+            $morocco_emp =  $morocco_emp +1 ;
+            $croatia_emp = $croatia_emp  +1  ;
+            $par = 3 ;
+        } else {
+            $morocco_pts = $morocco_pts + 3;
+            $morocco_win = $morocco_win + 1 ;
+            $croatia_lost = $croatia_lost  + 1 ;
+            $par = 3 ;
+        }
+        // Game 2
+        if ($input3_belgium_game1 < $input4_canda_game1) {
+            $canada_pts = $canada_pts + 3;
+            $canada_win = $canada_win + 1 ;
+            $belgium_lost = $belgium_lost  + 1 ;
+            
+        } else if ($input3_belgium_game1 == $input4_canda_game1) {
+            $belgium_pts = $belgium_pts + 1;
+            $canada_pts = $canada_pts + 1;
+            $belgium_emp =  $belgium_emp +1 ;
+            $canada_emp = $canada_emp  +1  ;
+        } else {
+            $belgium_pts = $belgium_pts + 3;
+            $belgium_win = $belgium_win + 1 ;
+            $canada_lost = $canada_lost  + 1 ;
+        }
+        // Game 3
+        if ($input5_belgium_game2 < $input6_morocco_game2) {
+            $morocco_pts = $morocco_pts + 3;
+            $morocco_win = $morocco_win + 1 ;
+            $belgium_lost = $belgium_lost  + 1 ;
+            
+        } else if ($input5_belgium_game2 == $input6_morocco_game2) {
+            $belgium_pts = $belgium_pts + 1;
+            $morocco_pts = $morocco_pts + 1;
+            $belgium_emp =  $belgium_emp +1 ;
+            $morocco_emp = $morocco_emp  +1  ;
+        } else {
+            $belgium_pts = $belgium_pts + 3;
+            $belgium_win = $belgium_win + 1 ;
+            $morocco_lost = $morocco_lost  + 1 ;
+        }
+        // Game 4
+        if ($input7_croatia_game2 < $input8_canada_game2) {
+            $canada_pts = $canada_pts + 3;
+            $canada_win = $canada_win + 1 ;
+            $croatia_lost = $croatia_lost  + 1 ;
+            
+        } else if ($input7_croatia_game2 == $input8_canada_game2) {
+            $canada_pts = $canada_pts + 1;
+            $croatia_pts = $croatia_pts + 1;
+            $canada_emp =  $canada_emp +1 ;
+            $croatia_emp = $croatia_emp  +1  ;
+        } else {
+            $croatia_pts = $croatia_pts + 3;
+            $croatia_win = $croatia_win + 1 ;
+            $canada_lost = $canada_lost  + 1 ;
+        }
+        // Game 5
+        if ($input9_croitia_game3 < $input10_belgium_game3) {
+            $belgium_pts = $belgium_pts + 3;
+            $belgium_win = $belgium_win + 1 ;
+            $croatia_lost = $croatia_lost  + 1 ;
+        } else if ($input9_croitia_game3 == $input10_belgium_game3) {
+            $belgium_pts = $belgium_pts + 1;
+            $croatia_pts = $croatia_pts + 1;
+            $belgium_emp =  $belgium_emp +1 ;
+            $croatia_emp = $croatia_emp  +1  ;
+        } else {
+            $croatia_pts = $croatia_pts + 3;
+            $croatia_win = $croatia_win + 1 ;
+            $belgium_lost = $belgium_lost  + 1 ;
+        }
+        // Game 6
+        if ($input11_canda_game3 < $input12_morocco_game3) {
+            $morocco_pts = $morocco_pts + 3;
+            $morocco_win = $morocco_win + 1 ;
+            $canada_lost = $canada_lost  + 1 ;
+            
+        } else if ($input11_canda_game3 == $input12_morocco_game3) {
+            $morocco_pts = $morocco_pts + 1;
+            $canada_pts = $canada_pts + 1;
+            $morocco_emp =  $morocco_emp +1 ;
+            $canada_emp = $canada_emp  +1  ;
+           
+        } else {
+            $canada_pts = $canada_pts + 3;
+            $canada_win = $canada_win + 1 ;
+            $morocco_lost = $morocco_lost  + 1 ;
+        }
+
+        // GF Calculation
+        $morocco_GF = $input1_morocco_game1 + $input6_morocco_game2 + $input12_morocco_game3 ;
+        $croatia_GF = $input2_crotia_game1 + $input7_croatia_game2 +$input9_croitia_game3 ;
+        $belgium_GF = $input3_belgium_game1 + $input5_belgium_game2 + $input10_belgium_game3 ;
+        $canada_GF = $input4_canda_game1 + $input8_canada_game2 + $input11_canda_game3;
+        // GC Calculation
+        $morocco_GC = $input2_crotia_game1 + $input5_belgium_game2 + $input11_canda_game3 ;
+        $croatia_GC = $input1_morocco_game1 + $input8_canada_game2 +$input10_belgium_game3 ;
+        $belgium_GC = $input4_canda_game1 + $input6_morocco_game2 + $input9_croitia_game3 ;
+        $canada_GC = $input3_belgium_game1 + $input7_croatia_game2 + $input1_morocco_game1;
+        // Goal Difference
+       $morocco_goal_dif =  $morocco_GF - $morocco_GC; 
+       $croatia_goal_dif =  $croatia_GF - $croatia_GC; 
+       $belgium_goal_dif =  $belgium_GF - $belgium_GC;
+       $canada_goal_dif  =  $canada_GF  - $canada_GC;
+  /////////// Table ////////////////////     
+     // Table header
+    echo "
+      <div  class='table-responsive{-sm|-md|-lg|-xl|-xxl} mt-4'> 
+      <table style=' border-collapse:separate;border-spacing:0 10px; ' class='table text-center align-middle table-bordered ' >
+      <thead style='border: #c2165e solid 1px;' > 
+        <tr> 
+          <th class='w-25'>#</th>
+          <th class='w-50'>Selection</th>
+          <th class='w-25'>PTS</th>
+          <th class='w-25'>PAR</th>
+          <th class='w-25'>GAN</th>
+          <th class='w-25'>EMP</th>
+          <th class='w-25'>PER</th>
+          <th class='w-25'>G.F</th>
+          <th class='w-25'>G.C</th> 
+          <th class='w-25'>+/-</th>
+        </tr>
+      </thead>";
+     //Stocking variables in Array 
+     //country flags 
+     $morocco_flag = "<img class='img-fluid w-25'  src='./img/mr.png'>";
+     $croitia_flag = "<img class='img-fluid w-25'  src='./img/cr.png'>";
+     $belgium_flag = "<img class='img-fluid w-25'  src='./img/blg.png'>";
+     $canada_flag = "<img class='img-fluid w-25'  src='./img/cnd.png'>";
+     // Array
+   $placement = array (
+    array("$morocco_flag Morocco",$morocco_pts,$par,$morocco_win,$morocco_emp,$morocco_lost,$morocco_GF,$morocco_GC,$morocco_goal_dif),
+    array("$croitia_flag Coatia",$croatia_pts,$par,$croatia_win,$croatia_emp,$croatia_lost,$croatia_GF,$croatia_GC,$croatia_goal_dif ),
+    array("$belgium_flag Belgium",$belgium_pts,$par,$belgium_win,$belgium_emp,$belgium_lost,$belgium_GF,$belgium_GC,$belgium_goal_dif),
+    array("$canada_flag Canada",$canada_pts,$par,$canada_win,$canada_emp,$canada_lost,$canada_GF,$canada_GC,$canada_goal_dif)
+  );
+   //  Array sorting ///////////////
+usort($placement, function($a, $b) {
+    if ($a[1] != $b[1]) {
+        return $b[1] <=> $a[1];
+    }
+    else if ($a[8] != $b[8]) {
+        return $b[8] <=> $a[8];
+    }
+    else if ($a[6] != $b[6]) {
+        return $b[6] <=> $a[6];
+    }
+    return 0;
+});
+    // Table body  
+    echo " <tbody style='border: #c2165e solid 1px;' class='text-light'>";
+    // Table tr
+  for ($row = 0; $row < 4; $row++) {
+    echo " <tr style='background-color: #870036;'><td>". $row +1 . "</td>";
+    // Table td
+    for ($col = 0; $col < 9; $col++) {
+   echo  "<td class=''>".  $placement[$row][$col] ."</td>"; 
+    }
+   echo "</tr> " ;
+  }   
+echo " </table></div>";
+}
+ ?>
+</div>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400&display=swap');
+
+        * {
+            font-family: 'Poppins', sans-serif;
+        }
+    </style>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+
+</html>
